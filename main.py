7b4f609779e3763cfdd834d91dc26dff534b8981
EricThanos <eric.leschinski@hotmail.com>
2025-01-19 13:00:42 -0500

    asdf

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def post_message_on_discord():
+    #get discord channel read and write functionality here
+    print("Done")
+
+post_message_on_discord()
+
+
