7f397b8d0b34f9219877334652a02e2fd6ccc88f
EricThanos <eric.leschinski@hotmail.com>
2025-01-19 13:16:17 -0500

    asdf

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 Post automated messages after criteria are met in a discord channel, read json from youtube events and behave given criteria, read discord responses, for artificial intelligent coded responses.  Python 3.10
 
+        $ python3 --version
+        Python 3.10.8
+
+        $ pip3 install --user discord-webhook
+        Requirement already satisfied: certifi>=2017.4.17 in /home/el/.local/lib/python3.10/site-packages (from requests<3.0.0,>=2.28.1->discord-webhook) (2022.9.24)
+        Installing collected packages: discord-webhook
+        Successfully installed discord-webhook-1.3.1
 
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from discord_webhook import DiscordWebhook
 
-def post_message_on_discord():
+def post_message_on_discord(msg):
     #get discord channel read and write functionality here
+
+    webhook_url = "https://discord.com/api/webhooks/1330601225682554991/vvTYfOY2lZQegnKLpEaFAJb_tCMtd2NJcMKcH9Bq3Fd95X9HVqsmZ6mWVRuCEHzdBPeT"
+    webhook = DiscordWebhook(url=webhook_url , content=msg)
+    response = webhook.execute()
+
+
     print("Done")
 
-post_message_on_discord()
+
+
+post_message_on_discord("This is a test message from thanos through webhook using DiscordWEbhook python 3.10.8")
 
 
